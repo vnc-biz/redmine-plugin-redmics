16425a70fd2d6ccb122449cce5b1810ed2c9947a
frank.schwarz@buschmais.com <frank.schwarz@buschmais.com@acf60f71-8f59-d6b5-87f9-fce14783422e>
2010-10-23 17:29:49 +0000

    preparing 1.0.2
    git-svn-id: http://redmics.googlecode.com/svn/trunk@12 acf60f71-8f59-d6b5-87f9-fce14783422e

--- init.rb
+++ init.rb
@@ -23,7 +23,7 @@ Redmine::Plugin.register :redmine_ics_export do
   name 'redmine ics export plugin (aka redmics)'
   author 'Frank Schwarz'
   description 'ICalendar view of issue- and version-deadlines'
-  version '1.0'
+  version '1.0.2'
   url 'http://code.google.com/p/redmics/'
   author_url 'http://www.buschmais.de/author/frank/'
 end
